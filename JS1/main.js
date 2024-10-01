658aeafe3d6fbfab082168188082eeeb5e2c528c
DizzyYT <153006021+VladKorotytskyi@users.noreply.github.com>
2024-10-01 20:15:55 +0200

    ///

---
+++ JS1/main.js
@@ -0,0 +1,74 @@
+'use strict'
+
+// var не використовуємо
+// let age = 20;
+// const x = 10000;
+// без ключового слова дає помилку в суворому режимі x = 10;
+
+
+// let username;
+// console.log(username);
+
+// age = 15;
+// console.log(age);
+
+// let age = 15;
+// age = 20;
+// console.log(age);
+
+// const age = 15;
+// console.log(age);
+
+// const price = 10;
+// const quantity = 3;
+// let total = price * quantity
+// console.log(total);
+// const priceNew = 50;
+
+// const x = 1;
+// const X = 1;
+
+// const COLOR = '#fff';
+
+
+
+// Типи Данних 
+// const age = 20; //number
+// const name = 'Vladyslav'; //string
+// const newAge = '20';
+
+// const isModalOpen = true; //boolean
+// const isLogged = false;
+
+// //null
+
+// let username;
+// console.log(username); //undefined
+
+// let username;
+// console.log(typeof username);
+
+
+// const name = 'Bob';
+// console.log(typeof name)
+
+// const age = 15;
+// console.log(typeof age);
+
+// const isModalOpen = false;
+// console.log(typeof isModalOpen);
+
+
+
+// console
+// const name = 'Vladyslav';
+// console.log('Username is ', name) //Username is Vladyslav
+
+// // const username = 'Hello, Vladyslav';
+// // alert(username);
+
+// // const isComing =confirm('Please confirm hotel reservation');
+// // console.log(isComing);
+
+// const username = prompt('Введіть своє імя');
+// console.log(username);
