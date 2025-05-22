a0ab91be5c62acbba13e10adde1e4300cc84a30b
DizzyYT <153006021+VladKorotytskyi@users.noreply.github.com>
2025-05-22 19:34:48 +0200

    ///

---
+++ НОВА ГРУППА/TODOLIST/main.js
@@ -0,0 +1,40 @@
+const formEl = document.getElementById("taskForm");
+const inputEl = document.getElementById("taskInput");
+const listEl = document.getElementById("taskList");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addTask(inputEl.value);
+  inputEl.value = "";
+});
+
+function addTask(text) {
+  const itemEl = document.createElement("li");
+
+  const checkboxEl = document.createElement("input");
+  checkboxEl.type = "checkbox";
+
+  const labelEl = document.createElement("span");
+  labelEl.textContent = text;
+
+  const deleteBtnEl = document.createElement("button");
+  deleteBtnEl.textContent = "Видалити";
+  deleteBtnEl.type = "button";
+
+  checkboxEl.addEventListener("change", () => {
+    if (checkboxEl.checked) {
+      labelEl.classList.add("completed");
+    } else {
+      labelEl.classList.remove("completed");
+    }
+  });
+
+  deleteBtnEl.addEventListener("click", () => {
+    itemEl.remove();
+  });
+
+  itemEl.appendChild(checkboxEl);
+  itemEl.appendChild(labelEl);
+  itemEl.appendChild(deleteBtnEl);
+  listEl.appendChild(itemEl);
+}
